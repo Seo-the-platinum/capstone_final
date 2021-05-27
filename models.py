a6b35f18e81181be1b4b470d0b66d74c47ffd8c2
Eliseo Flores <eliseo_flores530@yahoo.com>
2021-05-26 19:55:17 -0700

    changed db path again

--- models.py
+++ models.py
@@ -6,7 +6,7 @@ import os
 '''
 database_path = 'postgresql://postgres:seoisoe5i73@localhost:5432/capstonedb'
 '''
-database_path = os.environ['DATABASE_URL']
+database_path = 'postgresql://krwebmmdajeviy:da2f0d787f042a8d73d475fbaeb33aa915100d967f646a232343e558022cfe40@ec2-34-206-8-52.compute-1.amazonaws.com:5432/d3d97kgror5tca'
 db = SQLAlchemy()
 '''
 setup_db(app)
